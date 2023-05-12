e16f4ccd7ca9af3b33bfc4a5b461ab69a00d05bd
Jeff Nyman <jeffnyman@gmail.com>
2023-05-12 04:57:33 -0500

    Reading high memory offsets.

--- src/entharion/memory.py
+++ src/entharion/memory.py
@@ -8,9 +8,14 @@ class Memory:
         self.static = self.read_word(0x0E)
         self.high = self.read_word(0x04)
 
+        self.routine_offset = self.read_word(0x28)
+        self.strings_offset = self.read_word(0x2A)
+
         print(f"Zcode version: {self.version}")
         print(f"Static memory start: {hex(self.static)}")
         print(f"High memory start: {hex(self.high)}")
+        print(f"Routine offset: {self.routine_offset}")
+        print(f"Strings offset: {self.strings_offset}")
 
     def read_byte(self, address) -> int:
         return self.data[address]
