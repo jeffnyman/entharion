496f0bbd70f9eb2a56ee0651ba5e50af163726cf
Jeff Nyman <jeffnyman@gmail.com>
2023-05-12 04:26:27 -0500

    Start of package; loading a zcode program.

--- src/entharion/__main__.py
+++ src/entharion/__main__.py
@@ -1,7 +1,26 @@
 import sys
 
 
+class Memory:
+    def __init__(self, data) -> None:
+        self.data = data
+
+
+class Loader:
+    @staticmethod
+    def load(filename) -> Memory:
+        f = open(filename, "rb")
+        data = f.read()
+
+        return Memory(data)
+
+
 def main() -> int:
+    zcode: Memory = Loader.load(sys.argv[1])
+
+    assert isinstance(zcode, Memory), "zcode must be instance of Memory"
+    assert isinstance(zcode.data, bytes), "zcode data must be of type bytes"
+
     return 0
 
 
