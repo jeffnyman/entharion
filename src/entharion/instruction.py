b56bfa5a9142aca95a59250e50ffa0fc55cf7389
Jeff Nyman <jeffnyman@gmail.com>
2023-05-12 07:39:27 -0500

    Getting the instruction form.

--- src/entharion/instruction.py
+++ src/entharion/instruction.py
@@ -8,7 +8,20 @@ class Instruction:
     def __init__(self, memory: "Memory", address: int) -> None:
         self.memory: "Memory" = memory
         self.address: int = address
+        self.opcode_byte: int
 
     def decode(self) -> None:
-        opcode_byte = self.memory.read_byte(self.address)
-        print(f"Opcode byte: {opcode_byte} ({hex(opcode_byte)})")
+        self.opcode_byte = self.memory.read_byte(self.address)
+        print(f"Opcode byte: {self.opcode_byte} ({hex(self.opcode_byte)})")
+
+        self._determine_form()
+
+    def _determine_form(self) -> None:
+        if self.memory.version >= 5 and self.opcode_byte == 0xBE:
+            print("FORM: Extended")
+        elif self.opcode_byte & 0b11000000 == 0b11000000:
+            print("FORM: Variable")
+        elif self.opcode_byte & 0b10000000 == 0b10000000:
+            print("FORM: Short")
+        else:
+            print("FORM: Long")
