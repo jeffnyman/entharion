5b0353cd722b9891ed737c7678d412d232b67251
Jeff Nyman <jeffnyman@gmail.com>
2023-05-12 04:36:42 -0500

    Adding basic memory reader methods.

--- src/entharion/memory.py
+++ src/entharion/memory.py
@@ -1,3 +1,9 @@
 class Memory:
     def __init__(self, data) -> None:
         self.data = data
+
+    def read_byte(self, address) -> int:
+        return self.data[address]
+
+    def read_word(self, address) -> int:
+        return (self.data[address] << 8) | self.data[address + 1]
