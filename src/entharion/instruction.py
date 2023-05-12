28769f2fd8d5e5f1ea4c05a616c2d106d6e47ce3
Jeff Nyman <jeffnyman@gmail.com>
2023-05-12 07:57:06 -0500

    Turning the form into a data type.

--- src/entharion/instruction.py
+++ src/entharion/instruction.py
@@ -3,25 +3,31 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from entharion.memory import Memory
 
+from enum import Enum
+
+Form = Enum("Form", "SHORT LONG VARIABLE EXTENDED")
+
 
 class Instruction:
     def __init__(self, memory: "Memory", address: int) -> None:
         self.memory: "Memory" = memory
         self.address: int = address
         self.opcode_byte: int
+        self.form: Form
 
     def decode(self) -> None:
         self.opcode_byte = self.memory.read_byte(self.address)
         print(f"Opcode byte: {self.opcode_byte} ({hex(self.opcode_byte)})")
 
         self._determine_form()
+        print(f"Instruction form: {self.form.name}")
 
     def _determine_form(self) -> None:
         if self.memory.version >= 5 and self.opcode_byte == 0xBE:
-            print("FORM: Extended")
+            self.form = Form.EXTENDED
         elif self.opcode_byte & 0b11000000 == 0b11000000:
-            print("FORM: Variable")
+            self.form = Form.VARIABLE
         elif self.opcode_byte & 0b10000000 == 0b10000000:
-            print("FORM: Short")
+            self.form = Form.SHORT
         else:
-            print("FORM: Long")
+            self.form = Form.LONG
