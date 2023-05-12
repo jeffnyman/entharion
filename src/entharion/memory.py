5c15370b175895295ed1ae89b13b872ed17a4ddd
Jeff Nyman <jeffnyman@gmail.com>
2023-05-12 04:41:36 -0500

    Reading the memory regions.

--- src/entharion/memory.py
+++ src/entharion/memory.py
@@ -2,6 +2,16 @@ class Memory:
     def __init__(self, data) -> None:
         self.data = data
 
+        self.version = self.read_byte(0x00)
+
+        self.dynamic = 0
+        self.static = self.read_word(0x0E)
+        self.high = self.read_word(0x04)
+
+        print(f"Zcode version: {self.version}")
+        print(f"Static memory start: {hex(self.static)}")
+        print(f"High memory start: {hex(self.high)}")
+
     def read_byte(self, address) -> int:
         return self.data[address]
 
